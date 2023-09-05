a487aa773a8238844b5d02b02d4713fe29828f56
ekennedy1 <ekennedy@tamu.edu>
2023-09-05 17:27:19 -0500

    Revert "book_collection 2"
    This reverts commit a40af99340c3df202456977f922c53eb737c532d.

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -7,10 +7,8 @@ Rails.application.routes.draw do
   get 'tasks/index'
   get 'tasks/new'
   get 'tasks/edit'
-  
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :books do
-  end
 
   # Defines the root path route ("/")
   # root "articles#index"
--- test_app/spec/unit/unit_spec.rb
+++ test_app/spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter', author: 'person', price: 5, published: '4/11/16')
+    described_class.new(title: 'harry potter')
   end
 
   it 'is valid with valid attributes' do
@@ -12,9 +12,6 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
-    subject.author = nil
-    subject.price = nil
-    subject.published = nil
     expect(subject).not_to be_valid
   end
 end
